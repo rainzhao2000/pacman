3ee5b784dd01f245335b6360c73522f6db62157b
rain <rainzhao2000@gmail.com>
2018-04-29 05:19:54 -0400

    changed package, added header

---
+++ src/pacman/GameManager.java
@@ -0,0 +1,16 @@
+/*
+ * Collaborators: Rain Zhao and Jacky Zhao
+ * Due Date: 1 Jun 2018
+ * Description: The game manager class is responsible for
+ */
+
+package pacman;
+
+public class GameManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
