1b67919e3c1bc74e2484dbc49ae077f7395003dd
Rain Zhao <rainzhao2000@gmail.com>
2018-05-03 09:23:21 -0400

    Created GameManager GUI

--- src/pacman/GameManager.java
+++ src/pacman/GameManager.java
@@ -1,16 +1,51 @@
 /*
- * Collaborators: Rain Zhao and Jacky Zhao
+ * Collaborators: Jacky Zhao and Rain Zhao
  * Due Date: 1 Jun 2018
- * Description: The game manager class is responsible for
+ * Description:
  */
 
 package pacman;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class GameManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private JFrame frmPacman;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameManager window = new GameManager();
+					window.frmPacman.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GameManager() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPacman = new JFrame();
+		frmPacman.setResizable(false);
+		frmPacman.setTitle("Pac-Man");
+		frmPacman.setSize(600, 800);
+		frmPacman.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
